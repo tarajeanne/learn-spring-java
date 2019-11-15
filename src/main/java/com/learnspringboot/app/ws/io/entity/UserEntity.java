eda2176f041bfe255cdc7d8cd3d6b9400826380b
tarajeanne <t.jeanne.patel@gmail.com>
2019-11-15 09:59:43 -0600

    added entity class

---
+++ src/main/java/com/learnspringboot/app/ws/io/entity/UserEntity.java
@@ -0,0 +1,71 @@
+package com.learnspringboot.app.ws.io.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity(name="users")
+public class UserEntity implements Serializable {
+  private static final long serialVersionUID = 5313493413859894403L;
+
+  @Id
+  @GeneratedValue
+  private long id;
+
+  @Column(nullable=false)
+  private String UserId;
+
+  @Column(nullable=false, length=50)
+  private String name;
+
+  @Column(nullable=false, length=50)
+  private String username;
+
+  @Column(nullable=false)
+  private String encryptedPassword;
+
+  public long getId() {
+    return this.id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getUserId() {
+    return this.UserId;
+  }
+
+  public void setUserId(String UserId) {
+    this.UserId = UserId;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getUsername() {
+    return this.username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getEncryptedPassword() {
+    return this.encryptedPassword;
+  }
+
+  public void setEncryptedPassword(String encryptedPassword) {
+    this.encryptedPassword = encryptedPassword;
+  }
+
+}
